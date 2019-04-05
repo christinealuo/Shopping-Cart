b6333c66618b95e3260d421454c08bcd29c53309
Christine Luo <christineluo@berkeley.edu>
2019-04-05 15:30:55 -0700

    finished

--- src/Product.js
+++ src/Product.js
@@ -5,21 +5,21 @@ class Product extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 1
+      // count: 1
     }
   }
 
-  addToCart() {
-    if (this.state.count > this.props.limit) {
-      alert(`There are too mnay ${this.props.productName} in your cart!`);
-    }
-    else {
-      this.setState({
-        count: this.state.count + 1
-      })
-      alert(`There are ${this.state.count} ${this.props.productName} in your cart!`);
-    }
-  }
+  // addToCart() {
+  //   if (this.state.count > this.props.limit) {
+  //     alert(`There are too many ${this.props.productName} in your cart!`);
+  //   }
+  //   else {
+  //     this.setState({
+  //       count: this.state.count + 1
+  //     })
+  //     alert(`There are ${this.state.count} ${this.props.productName} in your cart!`);
+  //   }
+  // }
 
   render() {
     return (
@@ -31,9 +31,13 @@ class Product extends React.Component {
             ${this.props.price}
           </div>
         </div>
-        <div className="ui bottom attached button" onClick={() => this.addToCart()}>
+        <div className="ui bottom attached button" onClick={() => {this.props.onAddToCart(this.props.productName, this.props.price)}}>
           <i className="add icon"></i>
           Add to Cart
+        </div>
+        <div className="ui bottom attached button" onClick={() => {this.props.onRemoveToCart(this.props.productName, this.props.price)}}>
+          <i className="remove icon"></i>
+          Remove from Cart
         </div>
       </div>
     </div>
